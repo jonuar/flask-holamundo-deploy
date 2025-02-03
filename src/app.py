c3e5f0eebff6fa9e005ea7733b08187cf5c4cb17
jonuar <joshuanunezarcila@gmail.com>
2025-02-03 13:20:38 +0100

    Proyecto de prueba listo para deployar

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    username = os.getenv("USER")
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+
+    print(username, email, password)
+
+    return "<h1>Mi primera app Flask deployada en Render</h1>"
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>", 404
+
+if __name__ == "__main__":
+    app.register_error_handler(404, status_404)
+    app.run(debug=True)
